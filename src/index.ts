4d6890052a8230a7b65f01c721b1570f694e43d8
Alfie Jones <alfie.jones@hotmail.co.uk>
2021-07-09 18:55:03 +0100

    Moved interface file into main file since microbundle wasn't picking them up

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default } from "./useTheme/index";
-export type { ThemeProviderProps } from "./provider/index.props";
 export { default as ThemeProvider } from "./provider/index";
--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,7 +1,37 @@
 import React, { useEffect, FC, useCallback, memo, useState } from "react";
 import Head from "next/head";
-import { ThemeProviderProps, UseThemeContext } from "./index.props";
-import Defaults from "./index.defaults";
+import Defaults from "./defaults.index";
+
+export type ThemeProviderProps = {
+  /** Use media query to toggle the theme between light and dark */
+  mediaQuery?: boolean;
+  /** Should we set the HTML attribute, defaults to true. If set to false handling the theme change is up to you */
+  setAttribute?: boolean;
+  /** The HTML attribute to be set. Defaults to class */
+  attribute?: `data-${string}` | "class";
+  /** List of all available theme names, defaults to the two props [lightTheme, darkTheme], eg ["lightTheme", "darkTheme"] */
+  themes?: string[];
+  /** Dark theme name, defaults to dark */
+  darkTheme?: string;
+  /** Light theme name, defaults to light */
+  lightTheme?: string;
+  /** Key used to store theme setting in localStorage */
+  storageKey?: string;
+  /** Default theme, defaults to light */
+  defaultTheme?: string;
+  /** The components children */
+  children?: React.ReactNode;
+};
+
+// This is the type for our context usage
+export type UseThemeContext = {
+  /** List of all available theme names, defaults to the two props [lightTheme, darkTheme], eg ["lightTheme", "darkTheme"] */
+  themes: string[];
+  /** The function to call when we want to change the theme */
+  handleChange: (theme: string) => void;
+  /** The current theme value */
+  value: string;
+};
 
 function handleTheme(theme: string, themes: string[], attribute: string) {
   const root = document.documentElement;
