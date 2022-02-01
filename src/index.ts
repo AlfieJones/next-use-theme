1c245e805a0ae60b3458983c6a8cb7efc16e06c1
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-02-01 19:25:55 +0000

    Moved to microbundle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as ThemeProvider } from "./provider";
 export * from "./provider";
-export { default } from "./useTheme";
+export { default as UseTheme } from "./useTheme";
 export * from "./useTheme";
