fe6af409d64befba393247bf375b322ff3f26185
Alfie Jones <32960904+AlfieJones@users.noreply.github.com>
2022-02-25 15:37:32 +0000

    updated example

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -2,7 +2,7 @@
 import "../styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
-import { ThemeProvider, urlStorage } from "../test";
+import { ThemeProvider, urlStorage } from "next-use-theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
